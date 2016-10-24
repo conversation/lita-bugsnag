9850c1ae17a642a2ad7390ef19823b6eb2ef5a90
James Healy <james@yob.id.au>
2016-10-24 14:40:44 +1100

    add some helpful methods to BugsnagEvent

--- lib/lita/bugsnag_event.rb
+++ lib/lita/bugsnag_event.rb
@@ -8,6 +8,46 @@ class BugsnagEvent
     @data = JSON.parse(data)
   end
 
+  def account_id
+    @data.fetch("account", {}).fetch("id", "")
+  end
+
+  def account_name
+    @data.fetch("account", {}).fetch("name", "")
+  end
+
+  def project_id
+    @data.fetch("project", {}).fetch("id", "")
+  end
+
+  def project_name
+    @data.fetch("project", {}).fetch("name", "")
+  end
+
+  def trigger_type
+    @data.fetch("trigger", {}).fetch("type", "")
+  end
+
+  def trigger_message
+    @data.fetch("trigger", {}).fetch("message", "")
+  end
+
+  def error_id
+    @data.fetch("error", {}).fetch("id", "")
+  end
+
+  def error_exception_class
+    @data.fetch("error", {}).fetch("exceptionClass", "")
+  end
+
+  def error_release_stage
+    @data.fetch("error", {}).fetch("releaseStage", "")
+  end
+
+  def error_url
+    @data.fetch("error", {}).fetch("url", "")
+  end
+
   def to_json
     JSON.pretty_generate(@data)
   end
--- spec/bugsnag_event_spec.rb
+++ spec/bugsnag_event_spec.rb
@@ -2,12 +2,73 @@ require 'lita/bugsnag_event'
 require 'json'
 
 describe BugsnagEvent do
-  let(:json) { JSON.dump(foo: 1) }
+  let(:json_path) { File.join(File.dirname(__FILE__), "fixtures", "exception.json")}
+  let(:json) { File.read(json_path) }
   let(:event) { BugsnagEvent.new(json)}
 
   describe 'to_json' do
     it "returns correct json" do
-      expect(event.to_json).to eq %Q{{\n  "foo": 1\n}}
+      expect(event.to_json).to match /\{.+\}/m
+    end
+  end
+
+  describe 'account_name' do
+    it "returns correct value" do
+      expect(event.account_name).to eq "Test Account"
+    end
+  end
+
+  describe 'account_id' do
+    it "returns correct value" do
+      expect(event.account_id).to eq "123"
+    end
+  end
+
+  describe 'project_name' do
+    it "returns correct value" do
+      expect(event.project_name).to eq "project"
+    end
+  end
+
+  describe 'project_id' do
+    it "returns correct value" do
+      expect(event.project_id).to eq "456"
+    end
+  end
+
+  describe 'trigger_type' do
+    it "returns correct value" do
+      expect(event.trigger_type).to eq "exception"
+    end
+  end
+
+  describe 'trigger_message' do
+    it "returns correct value" do
+      expect(event.trigger_message).to eq "Event"
+    end
+  end
+
+  describe 'error_id' do
+    it "returns correct value" do
+      expect(event.error_id).to eq "789"
+    end
+  end
+
+  describe 'error_exception_class' do
+    it "returns correct value" do
+      expect(event.error_exception_class).to eq "ActionController::ParameterMissing"
+    end
+  end
+
+  describe 'error_release_stage' do
+    it "returns correct value" do
+      expect(event.error_release_stage).to eq "production"
+    end
+  end
+
+  describe 'error_url' do
+    it "returns correct value" do
+      expect(event.error_url).to eq "https://app.bugsnag.com/test-account/project/errors/1234?event_id=789"
     end
   end
 end
